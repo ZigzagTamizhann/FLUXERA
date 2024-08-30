07bac473fbd108f71b2010e806b40ab26d7f28f3
ZigzagTamizhann <114802656+ZigzagTamizhann@users.noreply.github.com>
2024-08-30 15:05:05 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+
+  const popup = document.getElementById('popup');
+  
+  const openPopup = () => {
+    popup.classList.add('open-popup');
+  };
+  
+  const closePopup = () => {
+    popup.classList.remove('open-popup');
+  };
+  
+  window.dataLayer = window.dataLayer || [];
+
+  
